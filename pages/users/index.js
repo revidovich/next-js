06d1e19e94a52025baf8738ef2f3e9834f9eb0f0
Anna Revidovich <Anna.Revidovich@lanit-tercom.com>
2022-11-07 22:29:33 +0500

    api

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,18 +1,39 @@
 import Link from "next/link";
 import Head from "next/head";
 import Heading from "../../components/Heading";
-
+import styles from "../../styles/List.module.scss";
+import { useState, useEffect } from "react";
 
 const Index = () => {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+      setUsers(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Head>
         <title>Users</title>
       </Head>
       <Heading text='List of users:' />
-      <Link href="./users/user">
-        User1
-      </Link>
+      <ul className={styles.list}>
+        {users && users.map(({ id, name, email }) => (
+          <li key={id}>
+            <strong>
+                {name}
+              <Link href="./users/user">
+                {name}
+              </Link>
+            </strong> ({email})
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
