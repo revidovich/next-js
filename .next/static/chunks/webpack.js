add9f785580c148be7ac97cd373c661506a04f5f
Anna Revidovich <Anna.Revidovich@lanit-tercom.com>
2022-11-10 00:48:00 +0500

    build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca3c39e59ec0f912"; }
+/******/ 		__webpack_require__.h = function() { return "2abda4392ca07e05"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
